3d1900d178f88c6933c913b1a7763addafbce95b
stefan <bulsis_br@abv.bg>
2016-03-17 11:38:46 +0200

    img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,21 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= Meta::meta('title'); ?></title>
-         <?= Meta::tag('description'); ?>
+        <?= Meta::tag('description'); ?>
         <meta name="keywords" content="Shop, Online, Магазин, Продукти, Промоция, Ниски цени, Пазаруване, Поръчки, Закупуване">
         <meta property="og:title" content="<?= Meta::meta('title'); ?>"/>
-         <meta property="og:description" content="<?= Meta::meta('description'); ?>"/>
-         <meta property="og:image" content="{{url("img/myimg.jpg")}}"/>
+        <meta property="og:description" content="<?= Meta::meta('description'); ?>"/>
+        <meta property="og:image" content="{{url("img/myimg.jpg")}}"/>
         <!-- Fonts -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
         <!-- Styles -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
         <link href="{{URL::asset('css/style.css')}}" rel="stylesheet">
         <link rel="shortcut icon" href="{{URL::asset('img/fiv.png')}}" />
-        
+
     </head>
     <body>
-         
+
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
             <div class="container">
                 <div class="navbar-header">
@@ -34,17 +34,17 @@
 
                 </div>
                 <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
-<!--                    <ul class="nav navbar-nav">
-                       @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                            <li class="@if(Request::segment(1)== $localeCode) overwrite @endif">
-                               
-                                <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
-                                    {{{ $properties['native'] }}}
-                                </a>
-                            </li>
-                            @endforeach
-                    </ul>-->
-                  <div style='display: none;'>{{ LaravelLocalization::setLocale($_lang) }}</div>
+                    <!--                    <ul class="nav navbar-nav">
+                                           @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                                                <li class="@if(Request::segment(1)== $localeCode) overwrite @endif">
+                                                   
+                                                    <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+                                                        {{{ $properties['native'] }}}
+                                                    </a>
+                                                </li>
+                                                @endforeach
+                                        </ul>-->
+                    <div style='display: none;'>{{ LaravelLocalization::setLocale($_lang) }}</div>
 
                     <ul class="nav navbar-nav center-element">
                         <li><a href="#about" class="telephone"><span class="glyphicon glyphicon-earphone"></span> 00359887462209</a></li>
@@ -78,8 +78,8 @@
 
                 <div id="links" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
                     <ul class="nav navbar-nav second-nav ">
-                         @foreach ($category as $cat )
-                        
+                        @foreach ($category as $cat )
+
                         <li  class="@if (Request::is($_lang.'/category/'.$cat->slug)) overwrite @endif"><a  href="{{url($_lang.DIRECTORY_SEPARATOR.'category/'.$cat->slug)}}">{{strtoupper($cat->title)}} </a></li>                      
                         @endforeach
                     </ul>
@@ -88,7 +88,7 @@
                         <li class="@if(Request::path()== $_lang.'/order')overwrite @endif"><a href="{{url($_lang.DIRECTORY_SEPARATOR.'order')}}">{{ trans('messages.order') }} </a></li> 
                         <li><a href="{{url($_lang.DIRECTORY_SEPARATOR.'order')}}"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
                         <li><a href="{{url($_lang.DIRECTORY_SEPARATOR.'order')}}"><div class="numberCircle">{{$_order}}</div></a></li>  
-                         
+
                     </ul>
 
 
@@ -97,13 +97,22 @@
         </nav>
 
         @yield('content')
-        
 
+        <nav class="navbar navbar-default  navbar-fixed-bottom" role="navigation">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col-md-4"><div class="cliente"><img src = "{{url("img/undo.png")}}" width="20px" height="20px"/> <span class="text-footer">В Prod Price Вие имате право да върнете стоката в рамките на 30 дни</span></div></div>
+                    <div class="col-md-4"><div class="cliente"><img src = "{{url("img/cc.png")}}" width="20px" height="20px"/> <span class="text-footer">Вие заплащате всички закупени стоки при доставката</span></div></div>
+                    <div class="col-md-4"><div class="cliente"><img src = "{{url("img/delivery.png")}}" width="20px" height="20px"/> <span class="text-footer">Знаем  че искаш да получиш доставката си бързо. Ние в Prod Price правим всичко възможно да я получите възможно най – бързо</span></div></div>
+                    
+                </div>
+            </div>
+        </nav>
         <!-- JavaScripts -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-         <script src="{{URL::asset('js/jquery.bpopup.min.js')}}"></script>
-         <script src="{{URL::asset('js/fast.js')}}"></script>
+        <script src="{{URL::asset('js/jquery.bpopup.min.js')}}"></script>
+        <script src="{{URL::asset('js/fast.js')}}"></script>
         <script type="text/javascript">
 $(window).load(function () {
     $('#myCarousel').carousel({
@@ -114,15 +123,17 @@ $('.carousel').carousel();
 
 
         </script>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1459902027590468";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-</script>
+        <div id="fb-root"></div>
+        <script>(function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id))
+                    return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1459902027590468";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
 
     </body>
 </html>
