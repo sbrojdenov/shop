725d72e1748fd7e30baeae6d658486607c2c0760
stefan <bulsis_br@abv.bg>
2016-03-22 17:04:59 +0200

    pr

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,30 +9,33 @@ use App\Category;
 use App\Http\Controllers\Controller;
 use Meta;
 
-
 class CategoryController extends Controller {
 
     public function index($slug) {
         $this->meta();
         $category = Category::where('slug', $slug)->first();
         session_start();
-        if(isset($_SESSION['view'])){
-           $promotion =$_SESSION['view'];
+        if (isset($_SESSION['view'])) {
+            $promotion = $_SESSION['view'];
         }
-        
-        $products = Product::where('categories_id', $category->id)->get();      
-        return view('category.index', compact('products', 'category','promotion'));
+
+        $products = Product::where('categories_id', $category->id)->get();
+        return view('category.index', compact('products', 'category', 'promotion'));
     }
 
-    public function product($slug) { 
+    public function product($slug) {
+        session_start();
+        if (isset($_SESSION['view'])) {
+            $promotion = $_SESSION['view'];
+        }
         $product = Product::where('slug', $slug)->first();
-        $this->metaProduct($product->title,$product->summary);
+        $this->metaProduct($product->title, $product->summary);
         $similar = Product::where('categories_id', $product->categories_id)->limit(4)->offset(1)->get();
-        return view('category.product',compact('product','similar'));
+        return view('category.product', compact('product', 'similar','promotion'));
     }
 
     public function order() {
-          $this->meta();
+        $this->meta();
         return view('category.order');
     }
 
@@ -40,15 +43,14 @@ class CategoryController extends Controller {
         $this->meta();
         return view('category.information');
     }
-    
-     private function meta(){
+
+    private function meta() {
         Meta::meta('title', 'Категория');
     }
-    
-    private function metaProduct($title,$des){
-          Meta::meta('title', $title);
-            Meta::meta('title', $des);
+
+    private function metaProduct($title, $des) {
+        Meta::meta('title', $title);
+        Meta::meta('title', $des);
     }
 
-   
 }
