e829ff3fcb78b7a70c7f5fa77b2671793c03bf92
stefan <bulsis_br@abv.bg>
2016-04-04 11:46:37 +0300

    slug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,14 +7,14 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Meta;
+
 class Controller extends BaseController {
 
     use AuthorizesRequests,
         DispatchesJobs,
         ValidatesRequests;
-    
-      public function __construct()
-    {
+
+    public function __construct() {
         # Default title
         Meta::title('Prodprice');
         Meta::meta('description', 'Добре дошли в Prodprice. Сайта за лесни и бързи онлайн поръчки. Ние предлгаме разнообразие от продукти в различни категории!');
@@ -23,29 +23,18 @@ class Controller extends BaseController {
         Meta::meta('robots', 'index,follow');
     }
 
-    public function slugify($text) {
-        // replace non letter or digits by -
-        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+    public function slugify($string) {
+        $mytext = $this->sluggable($string);
+        $data = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $mytext)));
 
-        // trim
-        $text = trim($text, '-');
-
-        // transliterate
-        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-        // lowercase
-        $text = strtolower($text);
-
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
-
-        if (empty($text)) {
-            return 'n-a';
-        }
-
-        return $text;
+        return substr($data, 0, 20);
     }
-    
-    
+
+    protected function sluggable($text) {
+        $myP = strtolower($text);
+        $find = array('а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ю', 'я');
+        $replace = array('a', 'b', 'v', 'g', 'd', 'e', 'j', 'z', 'i', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'i', 'f', 'h', 'c', 'ch', 'sh', 'sh', 'y', 'ju', 'q');
+        return str_replace($find, $replace, $myP);
+    }
 
 }
